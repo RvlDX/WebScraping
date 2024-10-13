2bc2f13fa060bf5a8b0e29a1b7af095bf5a9107a
Revolver_DX <114694204+RvlDX@users.noreply.github.com>
2024-10-13 13:55:31 +0700

    Update api.php

--- php/api.php
+++ php/api.php
@@ -1,1 +1,16 @@
+<?php
+header('Content-Type: application/json');
 
+// Contoh data yang diambil dari hasil scraping
+$anime_data = [
+    [
+        'title' => 'Anime 1',
+        'description' => 'Deskripsi anime 1',
+        'link' => 'https://streaming-link.com/anime1',
+        'genre' => ['Action', 'Adventure']
+    ],
+    // Tambahkan data lainnya
+];
+
+echo json_encode($anime_data);
+?>
